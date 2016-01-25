f07fedcc68825e45ea002d32ba5ee0c361877191
Johannes Zellner <johannes@nebulon.de>
2016-01-25 18:25:21 +0100

    Make app work

--- Dockerfile
+++ Dockerfile
@@ -8,6 +8,10 @@ WORKDIR /app/code
 
 RUN apt-get update && apt-get install -y sogo
 
+ADD sogo.conf /app/code/sogo.conf
+ADD nginx.conf /app/code/nginx.conf
 ADD start.sh /app/code/start.sh
+
+RUN rm /etc/sogo/sogo.conf && ln -s /run/sogo.conf /etc/sogo/sogo.conf
 
 CMD [ "/app/code/start.sh" ]
--- start.sh
+++ start.sh
@@ -4,4 +4,21 @@ set -eux
 
 echo "SOGo"
 
-read
+sed -e "s,##MYSQL_URL##,${MYSQL_URL}," \
+    -e "s,##MAIL_SMTP_SERVER##,${MAIL_SMTP_SERVER}," \
+    -e "s,##LDAP_URL##,${LDAP_URL}," \
+    -e "s/##LDAP_BIND_DN##/${LDAP_BIND_DN}/" \
+    -e "s/##LDAP_BIND_PASSWORD##/${LDAP_BIND_PASSWORD}/" \
+    -e "s/##LDAP_USERS_BASE_DN##/${LDAP_USERS_BASE_DN}/" \
+    /app/code/sogo.conf  > /run/sogo.conf
+
+sed -e "s,##HOSTNAME##,${APP_DOMAIN}," \
+    /app/code/nginx.conf  > /run/nginx.conf
+
+chown cloudron:cloudron /run
+
+echo "Start nginx"
+nginx -c /run/nginx.conf &
+
+echo "Start sogod"
+/usr/local/bin/gosu cloudron:cloudron /usr/sbin/sogod
