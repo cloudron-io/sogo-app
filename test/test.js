e606ac21a67a9876776411a06f6ffc3784c62d1b
Johannes Zellner <johannes@nebulon.de>
2016-02-26 17:33:57 +0100

    Finish basic tests

--- test/test.js
+++ test/test.js
@@ -40,16 +40,15 @@ describe('Application life cycle test', function () {
     });
 
     var LOCATION = 'rctest';
-    var TEST_MESSAGE = 'Hello Test!';
-    var TEST_CHANNEL = 'general';
-    var TEST_TIMEOUT = 5000;
+    var EVENT_TITLE = 'Meet the Cloudron Founders';
+    var TEST_TIMEOUT = 10000;
     var app;
 
-    xit('build app', function () {
+    it('build app', function () {
         execSync('cloudron build', { cwd: path.resolve(__dirname, '..'), stdio: 'inherit' });
     });
 
-    xit('install app', function () {
+    it('install app', function () {
         execSync('cloudron install --new --wait --location ' + LOCATION, { cwd: path.resolve(__dirname, '..'), stdio: 'inherit' });
     });
 
@@ -63,56 +62,76 @@ describe('Application life cycle test', function () {
 
     it('can login', function (done) {
         browser.get('https://' + app.fqdn);
+
         browser.wait(until.elementLocated(by.id('input_1')), TEST_TIMEOUT).then(function () {
             browser.wait(until.elementIsVisible(browser.findElement(by.id('input_1'))), TEST_TIMEOUT).then(function () {
                 browser.findElement(by.id('input_1')).sendKeys(process.env.USERNAME);
                 browser.findElement(by.id('input_2')).sendKeys(process.env.PASSWORD);
                 browser.findElement(by.name('loginForm')).submit();
-                browser.wait(until.elementLocated(by.xpath('/html/body/main/section/div/div[1]/md-content/button')), TEST_TIMEOUT).then(function () { done(); });
+                browser.wait(until.elementLocated(by.xpath('//*[@aria-label="Create a new event"]')), TEST_TIMEOUT).then(function () { done(); });
             });
         });
     });
 
-    xit('can join channel', function (done) {
-        browser.get('https://' + app.fqdn + '/channel/' + TEST_CHANNEL);
-        browser.wait(until.elementLocated(by.name('msg')), TEST_TIMEOUT).then(function () { done(); });
-    });
+    it('can create event', function (done) {
+        browser.get('https://' + app.fqdn + '/SOGo/so/' + process.env.USERNAME + '/Calendar/view');
 
-    xit('can send message', function (done) {
-        browser.get('https://' + app.fqdn + '/channel/' + TEST_CHANNEL);
-        browser.wait(until.elementLocated(by.name('msg')), TEST_TIMEOUT).then(function () {
-            browser.findElement(by.name('msg')).sendKeys(TEST_MESSAGE);
-            browser.findElement(by.name('msg')).sendKeys(Keys.RETURN);
-            browser.wait(browser.findElement(by.xpath("//*[contains(text(), '" + TEST_MESSAGE + "')]")), TEST_TIMEOUT).then(function () { done(); });
+        browser.wait(until.elementLocated(by.xpath('//*[@aria-label="Create a new event"]')), TEST_TIMEOUT).then(function () {
+            browser.wait(until.elementIsVisible(browser.findElement(by.xpath('//*[@aria-label="Create a new event"]'))), TEST_TIMEOUT).then(function () {
+                browser.findElement(by.xpath('//*[@aria-label="Create a new event"]')).click();
+
+                browser.wait(until.elementLocated(by.xpath('//*[@ng-model="editor.component.summary"]')), TEST_TIMEOUT).then(function () {
+                    browser.wait(until.elementIsVisible(browser.findElement(by.xpath('//*[@ng-model="editor.component.summary"]'))), TEST_TIMEOUT * 10).then(function () {
+                        browser.findElement(by.xpath('//*[@ng-model="editor.component.summary"]')).sendKeys(EVENT_TITLE);
+                        browser.findElement(by.xpath('//*[@ng-model="editor.component.summary"]')).submit();
+
+                        browser.wait(until.elementLocated(by.xpath('//*[text()="' + EVENT_TITLE + '"]')), TEST_TIMEOUT).then(function () {
+                            done();
+                        });
+                    });
+                });
+            });
         });
     });
 
-    xit('backup app', function () {
+    it('event is present', function (done) {
+        browser.get('https://' + app.fqdn + '/SOGo/so/' + process.env.USERNAME + '/Calendar/view');
+
+        browser.wait(until.elementLocated(by.xpath('//*[text()="' + EVENT_TITLE + '"]')), TEST_TIMEOUT).then(function () {
+            done();
+        });
+    });
+
+    it('backup app', function () {
         execSync('cloudron backup --app ' + app.id, { cwd: path.resolve(__dirname, '..'), stdio: 'inherit' });
     });
 
-    xit('restore app', function () {
+    it('restore app', function () {
         execSync('cloudron restore --app ' + app.id, { cwd: path.resolve(__dirname, '..'), stdio: 'inherit' });
     });
 
-    xit('can login', function (done) {
-        browser.get('https://' + app.fqdn + '/home');
-        browser.wait(until.elementLocated(by.name('emailOrUsername')), TEST_TIMEOUT).then(function () {
-            browser.findElement(by.name('emailOrUsername')).sendKeys(process.env.USERNAME);
-            browser.findElement(by.name('pass')).sendKeys(process.env.PASSWORD);
-            browser.findElement(by.id('login-card')).submit();
-            browser.wait(until.elementLocated(by.className('room-title')), TEST_TIMEOUT).then(function () { done(); });
+    it('can login', function (done) {
+        browser.get('https://' + app.fqdn);
+
+        browser.wait(until.elementLocated(by.id('input_1')), TEST_TIMEOUT).then(function () {
+            browser.wait(until.elementIsVisible(browser.findElement(by.id('input_1'))), TEST_TIMEOUT).then(function () {
+                browser.findElement(by.id('input_1')).sendKeys(process.env.USERNAME);
+                browser.findElement(by.id('input_2')).sendKeys(process.env.PASSWORD);
+                browser.findElement(by.name('loginForm')).submit();
+                browser.wait(until.elementLocated(by.xpath('//*[@aria-label="Create a new event"]')), TEST_TIMEOUT).then(function () { done(); });
+            });
         });
     });
 
-    xit('message is still there', function (done) {
-        browser.get('https://' + app.fqdn + '/channel/' + TEST_CHANNEL);
-        browser.wait(until.elementLocated(by.name('msg')), TEST_TIMEOUT).then(function () {
-            browser.wait(browser.findElement(by.xpath("//*[contains(text(), '" + TEST_MESSAGE + "')]")), TEST_TIMEOUT).then(function () { done(); });
+    it('event is still present', function (done) {
+        browser.get('https://' + app.fqdn + '/SOGo/so/' + process.env.USERNAME + '/Calendar/view');
+
+        browser.wait(until.elementLocated(by.xpath('//*[text()="' + EVENT_TITLE + '"]')), TEST_TIMEOUT).then(function () {
+            done();
         });
     });
 
-    xit('move to different location', function () {
+    it('move to different location', function () {
         browser.manage().deleteAllCookies();
         execSync('cloudron install --location ' + LOCATION + '2', { cwd: path.resolve(__dirname, '..'), stdio: 'inherit' });
         var inspect = JSON.parse(execSync('cloudron inspect'));
@@ -120,24 +139,28 @@ describe('Application life cycle test', function () {
         expect(app).to.be.an('object');
     });
 
-    xit('can login', function (done) {
-        browser.get('https://' + app.fqdn + '/home');
-        browser.wait(until.elementLocated(by.name('emailOrUsername')), TEST_TIMEOUT).then(function () {
-            browser.findElement(by.name('emailOrUsername')).sendKeys(process.env.USERNAME);
-            browser.findElement(by.name('pass')).sendKeys(process.env.PASSWORD);
-            browser.findElement(by.id('login-card')).submit();
-            browser.wait(until.elementLocated(by.className('room-title')), TEST_TIMEOUT).then(function () { done(); });
+    it('can login', function (done) {
+        browser.get('https://' + app.fqdn);
+
+        browser.wait(until.elementLocated(by.id('input_1')), TEST_TIMEOUT).then(function () {
+            browser.wait(until.elementIsVisible(browser.findElement(by.id('input_1'))), TEST_TIMEOUT).then(function () {
+                browser.findElement(by.id('input_1')).sendKeys(process.env.USERNAME);
+                browser.findElement(by.id('input_2')).sendKeys(process.env.PASSWORD);
+                browser.findElement(by.name('loginForm')).submit();
+                browser.wait(until.elementLocated(by.xpath('//*[@aria-label="Create a new event"]')), TEST_TIMEOUT).then(function () { done(); });
+            });
         });
     });
 
-    xit('message is still there', function (done) {
-        browser.get('https://' + app.fqdn + '/channel/' + TEST_CHANNEL);
-        browser.wait(until.elementLocated(by.name('msg')), TEST_TIMEOUT).then(function () {
-            browser.wait(browser.findElement(by.xpath("//*[contains(text(), '" + TEST_MESSAGE + "')]")), TEST_TIMEOUT).then(function () { done(); });
+    it('event is still present', function (done) {
+        browser.get('https://' + app.fqdn + '/SOGo/so/' + process.env.USERNAME + '/Calendar/view');
+
+        browser.wait(until.elementLocated(by.xpath('//*[text()="' + EVENT_TITLE + '"]')), TEST_TIMEOUT).then(function () {
+            done();
         });
     });
 
-    xit('uninstall app', function () {
+    it('uninstall app', function () {
         execSync('cloudron uninstall --app ' + app.id, { cwd: path.resolve(__dirname, '..'), stdio: 'inherit' });
     });
 });
